d5a33e666cda76bcfd009cfc19e745c0227b43eb
Chris <leaderc@oregonstate.edu>
2024-10-28 14:00:34 -0400

    Time: 0 ms (100.00%), Space: 17.9 MB (18.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         previous = {}
-        
         for index, item in enumerate(nums):
             value = target - item
             if value in previous:
