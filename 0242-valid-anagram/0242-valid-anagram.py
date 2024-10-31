7fda17d0371ac6fe3d3adda9cbb5fdc536e61e8a
Chris <leaderc@oregonstate.edu>
2024-10-31 08:24:59 -0400

    Time: 15 ms (49.65%), Space: 17.6 MB (5.70%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        annie = list(s)
-        o_tay = list(t)
-        annie_s = sorted(annie)
-        o_tay_s = sorted(o_tay)
-        if annie_s != o_tay_s:
-            return False
-        else:
-            return True
+        return sorted(s) == sorted(t)
