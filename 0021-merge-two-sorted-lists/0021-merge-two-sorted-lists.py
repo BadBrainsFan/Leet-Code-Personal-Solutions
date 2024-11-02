0ee4b8f5acf3b830ad3ae6bcf1b5248a36821449
Chris <leaderc@oregonstate.edu>
2024-11-02 13:45:12 -0400

    Time: 0 ms (100.00%), Space: 16.5 MB (57.92%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -14,9 +14,10 @@ class Solution:
             else:
                 tail.next = list2
                 list2 = list2.next
-            tail = tail.next
+            tail = tail.next  
         if list1:
             tail.next = list1
         elif list2:
             tail.next = list2
         return dummy.next
+    
