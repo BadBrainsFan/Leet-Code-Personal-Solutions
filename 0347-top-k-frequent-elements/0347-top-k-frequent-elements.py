4eaeae42ef9f845425360fde47f3c1dc1e7587ff
Chris <leaderc@oregonstate.edu>
2024-11-06 13:38:59 -0500

    Time: 4 ms (72.06%), Space: 23.7 MB (6.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,8 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        bucket = [[] for _ in nums]
-
-        # O(N)
-        c = collections.Counter(nums)
-
-        # O(d) where d is the number of distinct numbers. d <= N
-        for num, freq in c.items():
+        count = {}
+        bucket = [[] for i in nums]
+        frequency = Counter(nums)
+        for num, freq in frequency.items():
             bucket[-freq].append(num)
-            
-        # O(?)
         return list(itertools.chain(*bucket))[:k]
