df4ae0719ed9cf4d1e4092566460f0e85d004dcf
Chris <leaderc@oregonstate.edu>
2024-11-09 14:34:27 -0500

    Time: 7 ms (39.74%), Space: 16.7 MB (33.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        colums = defaultdict(set)
         rows = defaultdict(set)
+        columns = defaultdict(set)
         squares = defaultdict(set)
         for row in range(9):
-            for colum in range(9):
-                if board[row][colum] == ".":
+            for column in range(9):
+                if (board[row][column] == "."):
                     continue
-                if (board[row][colum] in rows[row] or
-                    board[row][colum] in colums[colum] or
-                    board[row][colum] in squares[(row//3, colum//3)]):
+                if (board[row][column] in rows[row] or
+                   board[row][column] in columns[column] or
+                   board[row][column] in squares[(row//3, column//3)]):
                     return False
-                colums[colum].add(board[row][colum])
-                rows[row].add(board[row][colum])
-                squares[(row//3, colum//3)].add(board[row][colum])
+                columns[column].add(board[row][column])
+                rows[row].add(board[row][column])
+                squares[(row//3, column//3)].add(board[row][column])
         return True
