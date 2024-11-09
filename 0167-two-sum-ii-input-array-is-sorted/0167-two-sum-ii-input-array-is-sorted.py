2475eac1b028ae19c928f9e80c409c66b205d1dc
Chris <leaderc@oregonstate.edu>
2024-11-09 11:19:41 -0500

    Time: 0 ms (100.00%), Space: 17.5 MB (97.29%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for index, item in enumerate(numbers):
+            value = target - item
+            if value in hashmap:
+                return [hashmap[value] + 1, index + 1]
+            else:
+                hashmap[item] = index
