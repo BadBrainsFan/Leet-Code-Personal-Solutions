b373f2f3942f3af8a1404a86f50c97d3f44da699
Chris <leaderc@oregonstate.edu>
2024-11-09 10:25:25 -0500

    Time: 7 ms (81.69%), Space: 16.9 MB (78.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,5 +5,5 @@ class Solution:
             if not s[left].isalnum(): left += 1; continue
             if not s[right].isalnum(): right -= 1; continue
             if s[left].lower() != s[right].lower(): return False
-            else: left += 1; right -= 1
+            left += 1; right -= 1
         return True
