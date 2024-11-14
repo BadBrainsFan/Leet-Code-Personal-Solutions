ce10814fd9420875afa983e90e09ead5bc7c6556
Chris <leaderc@oregonstate.edu>
2024-11-14 11:14:18 -0500

    Time: 7 ms (39.62%), Space: 16.9 MB (10.52%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -5,11 +5,11 @@ class Solution:
         squares = defaultdict(set)
         for row in range(9):
             for column in range(9):
-                if (board[row][column] == "."):
+                if board[row][column] == ".":
                     continue
                 if (board[row][column] in rows[row] or
-                   board[row][column] in columns[column] or
-                   board[row][column] in squares[(row//3, column//3)]):
+                    board[row][column] in columns[column] or
+                    board[row][column] in squares[(row//3, column//3)]):
                     return False
                 rows[row].add(board[row][column])
                 columns[column].add(board[row][column])
