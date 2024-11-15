94b06b461ac6e5ced57e5ac4718c16ffd8d64d1e
Chris <leaderc@oregonstate.edu>
2024-11-15 18:48:13 -0500

    Time: 157 ms (56.31%), Space: 36.1 MB (89.11%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,10 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position,speed), reverse = True)
+        cars = sorted(zip(position, speed), reverse = True)
         fleets = 0
         prev_time = -1.0
         for pos, spd in cars:
-            time = (target-pos) / spd
+            time = (target - pos) / spd
             if time > prev_time:
                 fleets += 1
                 prev_time = time
