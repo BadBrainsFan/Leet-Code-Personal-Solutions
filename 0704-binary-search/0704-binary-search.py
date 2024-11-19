5981ae6e99dbd8481174e594398dfa939327d114
Chris <leaderc@oregonstate.edu>
2024-11-18 19:06:46 -0500

    Time: 0 ms (100.00%), Space: 17.7 MB (74.48%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,8 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums)-1
+        left, right = 0, len(nums) - 1
         while left <= right:
-            mid_point = left + ((right-left)//2)
+            mid_point = left + ((right- left)//2)
             if nums[mid_point] > target:
                 right = mid_point - 1
             elif nums[mid_point] < target:
@@ -10,3 +10,4 @@ class Solution:
             else:
                 return mid_point
         return -1
+                
