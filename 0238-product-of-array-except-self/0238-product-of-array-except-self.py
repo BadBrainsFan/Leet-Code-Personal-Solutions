ccf79c06a155d35c41aea8f0309b9cef9a0911b5
Chris <cleader@byupathway.edu>
2025-07-07 17:47:40 -0400

    Time: 17 ms (92.67%), Space: 23.3 MB (54.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,10 +5,10 @@ class Solution:
         prefix_product = 1
         for i in range(len(nums)):
             answer[i] = prefix_product
-            prefix_product *=  nums[i]
+            prefix_product *= nums[i]
         
         suffix_product = 1
-        for i in range(len(nums)-1, -1, -1):
+        for i in range(len(nums) -1, -1, -1):
             answer[i] *= suffix_product
             suffix_product *= nums[i]
         
