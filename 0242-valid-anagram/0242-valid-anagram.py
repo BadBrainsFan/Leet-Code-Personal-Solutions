2a17a822e0df0be097242eee401d39a072b61f3d
Chris <cleader@byupathway.edu>
2025-07-25 15:28:13 -0400

    Time: 15 ms (41.74%), Space: 18.6 MB (11.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        count_s = {}
-        for item in s:
-            if item not in count_s:
-                count_s[item] = 1
-            else:
-                count_s[item] += 1
-
-        count_t = {}
-        for item in t:
-            if item not in count_t:
-                count_t[item] = 1
-            else:
-                count_t[item] += 1
-        
-        return count_s == count_t
+        return sorted(s) == sorted(t)
