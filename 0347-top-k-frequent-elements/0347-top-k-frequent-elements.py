7bd966c7d3f205d73f2b30611424843e55e9f6b2
Chris <cleader@byupathway.edu>
2025-07-29 18:59:49 -0400

    Time: 8 ms (33.92%), Space: 21.1 MB (90.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,7 +4,7 @@ class Solution:
         answer = []
         for num in nums:
             count_dict[num] = 1 + count_dict.get(num, 0)
-        sorted_count_dict = dict(sorted(count_dict.items(), key=lambda item:item[1], reverse=True))
+        sorted_count_dict = dict(sorted(count_dict.items(), key = lambda item:item[1], reverse = True))
         for key in sorted_count_dict.keys():
             answer.append(key)
         return answer[:k]
