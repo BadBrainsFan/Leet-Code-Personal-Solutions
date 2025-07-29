657ab84a82897a40c4c665c48f2cd480bdcdb0e0
Chris <cleader@byupathway.edu>
2025-07-29 17:44:16 -0400

    Time: 11 ms (82.6%), Space: 21.4 MB (38.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,6 +2,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         answer = defaultdict(list)
         for word in strs:
-            sorted_word_tuple = tuple(sorted(word))
-            answer[sorted_word_tuple].append(word)
+            sorted_tuple_word = tuple(sorted(word))
+            answer[sorted_tuple_word].append(word)
         return list(answer.values())
